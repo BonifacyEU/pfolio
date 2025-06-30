ed05060315a03d47a54f51d38196071de5dac091
BonifacyEU <leszekbiskup90@gmail.com>
2025-06-30 16:54:53 +0200

    Split into 2 separate functions

--- converter.py
+++ converter.py
@@ -2,11 +2,13 @@ import csv
 import requests
 from datetime import datetime
 
-def save_rate(cur_code):
-    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+def get_rate(cur_code):
     response_currency = requests.get(f'https://api.nbp.pl/api/exchangerates/rates/A/{cur_code}/?format=json')
     currency_rate = response_currency.json()['rates'][0]["mid"]
+    return currency_rate
 
+def save_rate(currency_rate, cur_code):
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     with open('currency.csv', 'a', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=';')
-        writer.writerow([timestamp, f"{currency_rate:.4f}".replace('.',',')])
+        writer.writerow([timestamp, f"{cur_code}",f"{currency_rate:.4f}".replace('.',',')])
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from converter import save_rate
+from converter import get_rate, save_rate
 
-currency = input('Enter currency code: ').upper()
-save_rate(currency)
+currency_code = input('Enter currency code (USD, GBP, EUR...): ').upper()
+currency_rate = get_rate(currency_code)
+save_rate(currency_rate, currency_code)
